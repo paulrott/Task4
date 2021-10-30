429841ece31cbed9005272dcaca3453e6d67a462
gromov.p <paulrott76@gmail.com>
2021-10-30 15:27:57 +0300

    Initial commit

---
+++ Task4.py
@@ -0,0 +1,11 @@
+first_name = "Pavel"
+last_name = "Gromov"
+age = "31"
+weight = "85"
+height = "186"
+white_space = ' '
+BMI = "24,86"
+sentence = "Hi! My name is " + first_name + white_space + last_name + ", I'm " + age + " years old. " \
+"My weight is "+ weight+"kg"+" "+ "and my height is"+white_space + height+ "cm." \
+                                                                          "My body mass index is"+ white_space+ BMI
+print(sentence)
